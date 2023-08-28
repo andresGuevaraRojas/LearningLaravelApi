4bfd7be25a37069293b26bf5e91a4a98dd7210b4
Andres Guevara <andy_jose_andres@hotmail.com>
2023-08-27 18:29:32 -0600

    Add categories endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+
+Route::controller(CategoryController::class)->group(function () {
+    Route::get('/categories', 'index');
+});
